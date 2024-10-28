61250ffb0eaebcd9bb04b27fdbb44f9796d9a756
Ivan <70844716+JohanPeper@users.noreply.github.com>
2024-10-28 19:35:09 +0300

    feat: add monster one view and animation

--- monsterOne.js
+++ monsterOne.js
@@ -5,29 +5,64 @@ export class MonsterOne {
     constructor(player) {
         this.player = player;
         this.healthPoint = new HealthPoint('MonsterOne');
+        this.appearance();
         this.damagePlayer(5, this.player);
     }
 
-    damagePlayer(amount, player) {
-        let monsterOneDamageInterval = setInterval(() => {
-            console.log(player);
+    appearance() {
+        this.monsterOneContainer = document.createElement('div');
+        this.monsterOneContainer.classList.add('monsterOneContainer');
+        document.querySelector('.container').append(this.monsterOneContainer);
+        this.monsterOneShape = document.createElement('div');
+        this.monsterOneShape.classList.add(`monsterOneShape`);
+        document.querySelector('.monsterOneContainer').append(this.monsterOneShape);
+        this.monsterOneArrow = document.createElement('div');
+        this.monsterOneArrow.classList.add('monsterOneArrow');
+        document.querySelector('.monsterOneContainer').append(this.monsterOneArrow);
+        document.querySelector('.monsterOneArrow').getAnimations()[0].finish();
+    }
 
+    damagePlayer(amount, player) {
+        let monsterOneDamageInterval = setInterval(async () => {
+            this.shoot();
+            this.reload();
+            await this.sleep(500);
             player.healthPointDamage(amount);
         }, 5000);
+
     }
 
     takeDamage(amount) {
         if (this.lastMoveCount == 0) {
             this.lastMoveCount += amount;
             amount *= 2;
-            this.healthPoint.healthPointDamage(amount); 
-            
+            this.healthPoint.healthPointDamage(amount);
+
         }
         else {
-        let damage = amount - this.lastMoveCount; 
-        damage *= 2;
-        this.healthPoint.healthPointDamage(damage);
-        this.lastMoveCount = amount;
+            let damage = amount - this.lastMoveCount;
+            damage *= 2;
+            this.healthPoint.healthPointDamage(damage);
+            this.lastMoveCount = amount;
+        }
     }
+
+    async reload() {
+        document.querySelector('.monsterOneShape').classList.add('monsterOneShoot');
+        await this.sleep(500);
+        document.querySelector('.monsterOneShape').classList.remove('monsterOneShoot');
+        document.querySelector('.monsterOneShape').getAnimations()[0].finish();
+        document.querySelector('.monsterOneShape').getAnimations()[0].play();
     }
+
+    sleep(ms) { // задержка для кода
+        return new Promise(resolve => setTimeout(resolve, ms))
+    }
+
+    shoot() {
+        document.querySelector('.monsterOneArrow').getAnimations()[0].finish();
+        document.querySelector('.monsterOneArrow').getAnimations()[0].play();
+    }
+
+
 }
